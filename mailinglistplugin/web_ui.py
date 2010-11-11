f510d2a01046c1ce91667de0d3c06ea494023471
Nick Piper <nick.piper@cgi.com>
2010-11-11 13:54:52 +0000

    Add pagination for conversations list. See #36
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/features/ticket-36-mailinglists@1673 484df14f-75d2-4b3f-8ba0-490e05069a01

--- mailinglistplugin/web_ui.py
+++ mailinglistplugin/web_ui.py
@@ -3,7 +3,7 @@ from trac.perm import IPermissionRequestor
 from trac.resource import Resource, IResourceManager
 from trac.config import BoolOption, IntOption, ListOption
 from trac.web.chrome import INavigationContributor, ITemplateProvider, \
-                            add_stylesheet, add_link, add_ctxtnav
+                            add_stylesheet, add_link, add_ctxtnav, prevnext_nav
 from trac.web.main import IRequestHandler
 from trac.timeline.api import ITimelineEventProvider
 from trac.util.translation import _
@@ -18,6 +18,8 @@ import pkg_resources
 
 class MailinglistModule(Component):
     implements(IRequestHandler, ITemplateProvider, INavigationContributor)
+
+    limit = IntOption("mailinglist","page_size",20,"Number of conversations to show per page")
     
     # ITemplateProvider methods
 
@@ -47,23 +49,53 @@ class MailinglistModule(Component):
             return True
 
     def process_request(self, req):
+
+        offset = int(req.args.get("offset",0))
+        
         mailinglists = [m for m in Mailinglist.select(self.env)
                         if "MAILINGLIST_VIEW" in req.perm(m.resource)]
+
         data = {"mailinglists": mailinglists,
+                "offset": offset,
+                "limit": self.limit,
                 "wrap_and_quote": wrap_and_quote}
-        for mailinglist in mailinglists:
-            add_ctxtnav(req,
-                        _("List: %s") % mailinglist.name,
-                        req.href.mailinglist(mailinglist.emailaddress))
+
+        #for mailinglist in mailinglists:
+        #    add_ctxtnav(req,
+        #                _("List: %s") % mailinglist.name,
+        #                req.href.mailinglist(mailinglist.emailaddress))
 
         if 'conversationid' in req.args:
             conversation = MailinglistConversation(self.env, req.args['conversationid'])
             data['conversation'] = conversation
+
+            add_link(req, 'up', req.href.mailinglist(conversation.mailinglist.emailaddress,offset=data['offset']),
+                     _("List of conversations"))
+
+            prevnext_nav(req, _("Newer conversation"), _("Older conversation"), 
+                         _("Back to list of conversations"))
+            
             return 'mailinglist_conversation.html', data, None
         elif 'listname' in req.args:
             mailinglist = Mailinglist.select_by_address(self.env,
                                                         req.args['listname'], localpart=True)
             data['mailinglist'] = mailinglist
+
+            if data['offset'] + data['limit'] < mailinglist.count_conversations():
+                add_link(req, 'next',
+                         req.href.mailinglist(mailinglist.emailaddress,offset=data['offset']+data['limit']),
+                         _("Older conversations"))
+
+            if offset > 0:
+                add_link(req, 'prev',
+                         req.href.mailinglist(mailinglist.emailaddress,offset=data['offset']-data['limit']),
+                         _("Newer conversations"))
+
+            add_link(req, 'up', req.href.mailinglist(), _("List of mailinglists"))
+
+            prevnext_nav(req, _("Newer conversations"), _("Older conversations"), 
+                         _("Back to Mailinglists"))
+
             return 'mailinglist_conversations.html', data, None
         else:
             return 'mailinglist_list.html', data, None
