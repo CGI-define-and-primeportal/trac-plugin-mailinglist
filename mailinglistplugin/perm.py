1be39bf348376fb77b110881dc62e1d54040ef36
Nick Piper <nick.piper@cgi.com>
2010-11-12 17:02:40 +0000

    Fix the permission checks - it had lost the ability to check on individual messages and conversations. Right now, we don't expose any way to remote view permission on a particular message, or to take away post permission from a particular conversation; but I want the check to work at least. See #36
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/features/ticket-36-mailinglists@1711 484df14f-75d2-4b3f-8ba0-490e05069a01

--- mailinglistplugin/perm.py
+++ mailinglistplugin/perm.py
@@ -2,27 +2,39 @@ from trac.perm import IPermissionRequestor, IPermissionPolicy
 from trac.core import Component, implements, TracError, Interface, ExtensionPoint
 
 from mailinglistplugin.api import MailinglistSystem
+from mailinglistplugin.model import MailinglistConversation, MailinglistMessage
 
 class MailinglistPermissionPolicy(Component):
     implements(IPermissionPolicy)
 
     # IPermissionPolicy methods
     def check_permission(self, action, username, resource, perm):
+
+        # maybe we need to reorganise a little, to factor up the
+        # "isinstance" business.
+        
         if action is "ATTACHMENT_VIEW":
             self.log.debug("Deciding if %s can do %s on %s", username, action, resource)
-            if resource.parent.realm == "mailinglist":
+            if resource and resource.parent and resource.parent.realm == "mailinglist":
                 return "MAILINGLIST_VIEW" in perm(resource.parent)
 
         elif action is "MAILINGLIST_VIEW":
             self.log.debug("Deciding if %s can do %s on %s", username, action, resource)
-            if resource.realm == "mailinglist":
+            # if no resource, then it's no to the general permissions table
+            if resource and resource.realm == "mailinglist":
                 if "TRAC_ADMIN" in perm:
                     return True
-                mailinglist = MailinglistSystem(self.env).get_instance_for_resource(resource)
-                if mailinglist.private == False:
+                instance = MailinglistSystem(self.env).get_instance_for_resource(resource)
+                if isinstance(instance, MailinglistMessage):
+                    return action in perm(instance.conversation.resource)
+                elif isinstance(instance, MailinglistConversation):
+                    return action in perm(instance.mailinglist.resource)
+                elif instance.private == False:
+                    # it's a mailinglist
                     return None # it's up to the general permissions table
-                elif mailinglist.private == True:
-                    return username in mailinglist.subscribers()
+                elif instance.private == True:
+                    # it's a mailinglist
+                    return username in instance.subscribers()
             
         elif action is "MAILINGLIST_POST":
             self.log.debug("Deciding if %s can do %s on %s", username, action, resource)            
@@ -30,13 +42,19 @@ class MailinglistPermissionPolicy(Component):
                 # In general, people can post...
                 return True
             if resource.realm == "mailinglist":
-                mailinglist = MailinglistSystem(self.env).get_instance_for_resource(resource)
-                if mailinglist.postperm == "OPEN":
+                if "TRAC_ADMIN" in perm:
                     return True
-                elif mailinglist.postperm == "RESTRICTED":
+                instance = MailinglistSystem(self.env).get_instance_for_resource(resource)
+                if isinstance(instance, MailinglistMessage):
+                    return action in perm(instance.conversation.resource)
+                elif isinstance(instance, MailinglistConversation):
+                    return action in perm(instance.mailinglist.resource)
+                elif instance.postperm == "OPEN":
+                    return True
+                elif instance.postperm == "RESTRICTED":
                     try:
-                        return mailinglist.subscribers()[username]['poster']
+                        return instance.subscribers()[username]['poster']
                     except KeyError, e:
                         return False
-                elif mailinglist.postperm == "MEMBERS":
-                    return username in mailinglist.subscribers()
+                elif instance.postperm == "MEMBERS":
+                    return username in instance.subscribers()
