84827b9a889b75eb4a603fd05693d0f251151da5
Pontus Enmark <pontus.enmark@cgi.com>
2011-03-07 14:28:22 +0000

    Make mailinglist list foldable again. See #644
    git-svn-id: file:///mnt/ramdisk/d4-svn/trunk@2546 484df14f-75d2-4b3f-8ba0-490e05069a01

--- mailinglistplugin/htdocs/mailinglist.js
+++ mailinglistplugin/htdocs/mailinglist.js
@@ -1,10 +1,7 @@
 jQuery(function($){
-    $('a.moreinfo').click(function(e){
-        $(this).parent().parent().next().toggleClass('hidden');
-        return false;
-    })
-    $('a.morebody').click(function(e){
-        $(this).next().toggleClass('hidden');
+    $('.moreinfo,.lessinfo').click(function(e) {
+        $(this).toggleClass('moreinfo lessinfo')
+        $(this).closest('tr').next().toggleClass('hidden');
         return false;
     })
 })
